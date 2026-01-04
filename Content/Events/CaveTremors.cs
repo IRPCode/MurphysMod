832cd6e44e9e8a37d55664c49d26b47a476cc6e0
IRPCode <email.example@gmail.com>
2026-01-04 13:03:58 -0500

    further work on tremors

--- Content/Events/CaveTremors.cs
+++ Content/Events/CaveTremors.cs
@@ -2,6 +2,7 @@ using System;
 using System.Diagnostics;
 using System.Linq;
 using Microsoft.Xna.Framework;
+using MurphysMod.Content.LuckHandlers;
 using ReLogic.Utilities;
 using Terraria;
 using Terraria.Audio;
@@ -18,21 +19,21 @@ namespace MurphysMod.Content
         public static bool TremorActive = false;
         public static readonly SoundStyle tremorL = new("MurphysMod/Assets/Audio/UndergroundTremorLarge")
         {
-            Volume = 1.5f,
+            Volume = 2f,
             Pitch = 0f,
             PitchVariance = 0f
         };
 
         public static readonly SoundStyle tremorM = new("MurphysMod/Assets/Audio/UndergroundTremorMedium")
         {
-            Volume = 1.5f,
+            Volume = 2f,
             Pitch = 0f,
             PitchVariance = 0f
         };
 
         public static readonly SoundStyle tremorS = new("MurphysMod/Assets/Audio/UndergroundTremorSmall")
         {
-            Volume = 1.5f,
+            Volume = 2f,
             Pitch = 0f,
             PitchVariance = 0f
         };
@@ -40,97 +41,185 @@ namespace MurphysMod.Content
         public static int tremorTick;
 
         public static int length;
-        public static int intensity;
-        public static int offset = 24; //only used for offsetting tick for files that don't immediately start
+        public static double intensity;
+        public static int tremorType;
+
+        public static double currentIntensity;
         public static int intensityPeak;
         public static int[] dusts = { DustID.Dirt, DustID.Stone, DustID.Mud, DustID.Clay, DustID.WoodFurniture, DustID.Corruption, DustID.Crimson, DustID.JungleGrass, DustID.Sand, DustID.Snow, DustID.Ice, DustID.Ash };
         public static int[] tileTypes = { TileID.Dirt, TileID.Stone, TileID.Mud, TileID.ClayBlock, TileID.WoodBlock, TileID.Ebonstone, TileID.Crimstone, TileID.JungleGrass, TileID.Sand, TileID.SnowBlock, TileID.IceBlock, TileID.Ash };
         public override void PostUpdate()
         {
             TremorActive = true;
+            
 
-            if(tremorTick > 60 * 15)
-                tremorTick = 0;
-
-            if (TremorActive)
+            if (Main.LocalPlayer.position.Y / 16 >= Main.rockLayer)
             {
-                tremorTick++;
-
-                int tremorType = Main.rand.Next(1, 4);
-
-                if (tremorTick == 1)
+                if (tremorTick > 60 * 15)
                 {
-                    SlotId tremorSound;
-                    switch (tremorType) //modifies circumstances that transform matrix is modified under
+                    tremorTick = 0;
+                    TremorShake.multiplyStrength = .5f;
+                    TremorActive = false;
+                }
+
+                if (TremorActive)
+                {
+                    tremorTick++;
+
+                    if (tremorTick == 1)
                     {
-                        case 1:
-                            tremorSound = SoundEngine.PlaySound(tremorS);
-                            intensity = 25;
-                            length = 315;
-                            intensityPeak = 135;
-                            break;
+                        tremorType = Main.rand.Next(1, 4);
+                        currentIntensity = 1;
+                        SlotId tremorSound;
+                        switch (tremorType) //modifies circumstances that transform matrix is modified under
+                        {
+                            case 1:
+                                tremorSound = SoundEngine.PlaySound(tremorS);
+                                intensity = 40;
+                                length = 315;
+                                intensityPeak = 135;
+                                TremorShake.shakeStrength = 1.25f;
+                                break;
 
-                        case 2:
-                            tremorSound = SoundEngine.PlaySound(tremorM);
-                            intensity = 50;
-                            length = 348;
-                            intensityPeak = 180;
-                            break;
+                            case 2:
+                                tremorSound = SoundEngine.PlaySound(tremorM);
+                                intensity = 75;
+                                length = 348;
+                                intensityPeak = 180;
+                                TremorShake.shakeStrength = 2.5f;
+                                break;
 
-                        case 3:
-                            tremorSound = SoundEngine.PlaySound(tremorL);
-                            intensity = 100;
-                            length = 690;
-                            intensityPeak = 420;
-                            break;
-                    }
-                    //TODO: figure out how to make the sound stop if the player teleports
+                            case 3:
+                                tremorSound = SoundEngine.PlaySound(tremorL);
+                                intensity = 125;
+                                length = 690;
+                                intensityPeak = 420;
+                                TremorShake.shakeStrength = 2.75f;
+                                break;
+                        }
+                        //TODO: figure out how to make the sound stop if the player teleports
                         //if(Main.LocalPlayer.position.Y / 16 >= Main.rockLayer){}
                         //tremorSound.Stop();
-                }
+                    }
 
-                for (int i = 0; i < intensity; i++)
-                {
-                    int x = (int)(Player.Center.X / 16) + Main.rand.Next(-60, 60);
-                    int y = (int)(Player.Center.Y / 16) + Main.rand.Next(-36, 36);
+                    if (CaveTremors.tremorTick < CaveTremors.intensityPeak && intensity >= currentIntensity)
+                        currentIntensity *= 1.1f;
+                    else
+                        currentIntensity *= .99f;
 
-                    Tile selectedTile = Framing.GetTileSafely(x, y);
-                    Tile belowtile = Framing.GetTileSafely(x, y + 1);
-
-                    if (tileTypes.Contains(selectedTile.TileType) && !belowtile.HasTile && selectedTile.HasTile)
+                    for (int i = 0; i < currentIntensity; i++)
                     {
-                        int index = Array.IndexOf(tileTypes, Framing.GetTileSafely(x, y).TileType);
+                        int x = (int)(Player.Center.X / 16) + Main.rand.Next(-60, 60);
+                        int y = (int)(Player.Center.Y / 16) + Main.rand.Next(-36, 36);
 
-                        Dust.NewDust(new Vector2(x * 16, y * 16), 16, 16, dusts[index], 0, 0, 100, default, 1f);
+                        Tile selectedTile = Framing.GetTileSafely(x, y);
+                        Tile belowtile = Framing.GetTileSafely(x, y + 1);
 
-                        //TODO: use tiletype array to find what dust to select, then do a check if air is by block, spawn dust, play sound, make screen shake, and then spawn small boulders or large depending on what intensity is selected on random
+                        if (tileTypes.Contains(selectedTile.TileType) && !belowtile.HasTile && selectedTile.HasTile)
+                        {
+                            int index = Array.IndexOf(tileTypes, Framing.GetTileSafely(x, y).TileType);
+
+                            double scale = 1f;
+
+                            bool spawnDebris = false;
+                            int chance = -1;
+                            int projectileType = -1;
+                            int damage = 15; //TODO: balance this, 150 does 1.5k dmg but not to enemies (double check this)
+
+                            Player player = Main.LocalPlayer;
+                            LuckHandler luckHandler = player.GetModPlayer<LuckHandler>();
+                            double luckVal = luckHandler.luckValue();
+
+                            switch (tremorType) //modifies circumstances that transform matrix is modified under and projectile types
+                            {
+                                case 1:
+                                    scale = Main.rand.Next(5, 16) / 10;
+                                    spawnDebris = false;
+                                    break;
+
+                                case 2:
+                                    scale = Main.rand.Next(5, 21) / 10;
+                                    spawnDebris = true;
+                                    chance = Main.rand.Next(1, 2001 - (int)(luckVal * 1000));
+                                    projectileType = ProjectileID.MiniBoulder;
+
+                                    //TODO: Make the ceiling drop stalagtites and mini boulders (only large at max bad luck), give player and NPCs slowness during tremor
+                                    break;
+
+                                case 3:
+                                    scale = Main.rand.Next(5, 26) / 10;
+                                    spawnDebris = true;
+                                    chance = Main.rand.Next(1, 2501 - (int)(luckVal * 1000));
+
+                                    if (chance <= 20)
+                                        projectileType = ProjectileID.MiniBoulder;
+
+                                    else if (chance <= 25)
+                                    {
+                                        projectileType = ProjectileID.Boulder;
+                                        damage = 20;
+                                    }
+                                        
+                                    else if (chance == 26 && luckVal >= .75f)
+                                    {
+                                        projectileType = ProjectileID.BouncyBoulder;
+                                        damage = 25;
+                                    }
+                                        
+                                    break;
+                                default:
+                                    Main.NewText("CaveTremors class threw an error.");
+                                break;
+                            }
+
+                            Dust.NewDust(new Vector2(x * 16, y * 16), 16, 16, dusts[index], 0, 0, 100, default, (float)scale);
+
+                            if (spawnDebris == true && chance <= 26)
+                            {
+                                int projID = Projectile.NewProjectile(
+                                default,
+                                new Vector2(x * 16f, (y + 1) * 16f),
+                                default,
+                                projectileType,
+                                (int)(damage * (luckVal + .5f)), //for damage balancing
+                                default,
+                                -1);
+
+                                Projectile proj = Main.projectile[projID];
+                                proj.hostile = true;
+                                proj.friendly = true;
+                                proj.tileCollide = true;
+                                proj.netUpdate = true;
+                            }
+
+                        }
                     }
                 }
-
-                //1 == small, 2 == medium, 3 == large
-                //Small starts at .4 seconds, climaxes at 2.25 seconds, ends at 5.25 seconds (315 total ticks)
-                //Medium starts at 0, climaxes at 3 seconds, and ends at 5.8 seconds (348 ticks)
-                //Large starts at .4 seconds, climaxes at 4.8 seconds, continues until 7 seconds, fades out until 11.5 seconds (690)
-
             }
-
-
         }
 
         public class TremorShake : ModSystem
         {
-            private float shakeStrength = 1f;
+            public static float shakeStrength;
 
-            private float multiplyStrength = 1f;
+            public static float multiplyStrength;
 
             public override void ModifyTransformMatrix(ref SpriteViewMatrix transform)
             {
-                if (CaveTremors.TremorActive == true && (tremorTick <= length) &&  Main.LocalPlayer.position.Y / 16 >= Main.rockLayer) //checks just in case if player teleports, only works underground
+                if (CaveTremors.TremorActive == true && (tremorTick <= length) && Main.LocalPlayer.position.Y / 16 >= Main.rockLayer) //checks just in case if player teleports, only works underground
                 {
                     float randX = Main.rand.Next((int)-shakeStrength, (int)shakeStrength);
                     float randY = Main.rand.Next((int)-shakeStrength, (int)shakeStrength);
 
-                    multiplyStrength *= 1.00005f;
+                    if (tremorTick == 1)
+                    {
+                        multiplyStrength *= shakeStrength;
+                    }
+
+                    if (CaveTremors.tremorTick < CaveTremors.intensityPeak)
+                        multiplyStrength *= 1.0005f;
+                    else
+                        multiplyStrength *= .99f;
 
                     Vector2 displaceScreen = new Vector2(randX, randY) * multiplyStrength;
 
@@ -141,6 +230,11 @@ namespace MurphysMod.Content
                         return;
                     }
 
+                    if (CaveTremors.tremorTick >= CaveTremors.length)
+                    {
+                        shakeStrength = 0f;
+                        CaveTremors.TremorActive = false;
+                    }
                 }
                 else
                 {
--- obj/Debug/net8.0/MurphysMod.AssemblyInfo.cs
+++ obj/Debug/net8.0/MurphysMod.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MurphysMod")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2258e201f9da3c9fea060a50ea326eccfa75fe8c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ea43d40e9f5ad4aa76ba4451fccbbc66ad5fbde7")]
 [assembly: System.Reflection.AssemblyProductAttribute("MurphysMod")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MurphysMod")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
